c037405f06329dca120d2f6f18705628cddff02d
guanxiux <33594471+guanxiux@users.noreply.github.com>
2018-09-11 21:50:27 +0800

    Basic function implemented

---
+++ igem_parts&rcsb_count.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+import urllib
+from bs4 import BeautifulSoup
+
+
+def rcsb(query):
+    query_text = """
+        <orgPdbQuery>
+        <queryType>org.pdb.query.simple.AdvancedKeywordQuery</queryType>
+        <keywords>%s</keywords>
+        </orgPdbQuery>""" % query
+    data = query_text.encode('utf-8')
+    headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    url = 'https://www.rcsb.org/pdb/rest/search'
+    req = urllib.request.Request(url, data=data, headers=headers)
+    result = urllib.request.urlopen(req).read()
+    count = str(result).count('\\n')
+    search_url = 'http://www.rcsb.org/pdb/search/navbarsearch.do?f=&q=%s' % query
+    return count, search_url
+
+
+def igem_parts(query):
+    # 因为igem的query service(看起来是10年的一个参赛项目)已经挂掉了, 没办法用API获得模糊搜索的结果, 目前爬取的是原网站.
+    url = 'http://parts.igem.org/Special:Search?search=%s' % query
+    headers = {
+        'accept': 'text / html, application / xhtml + xml, application / xml; q = 0.9, image / webp, image / apng, * / *;q = 0.8'
+    }
+    req = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(html, 'lxml')
+    div = soup.find(attrs={'class': 'results-info'})
+    count = int(div.find_all('strong')[-1].text)
+    search_url = 'http://parts.igem.org/Special:Search?search=%s' % query
+    return count, search_url
+
+
+if __name__=='__main__':
+    print(rcsb('p53'))
+    print(igem_parts('p53'))
