21413035d1956f8879c0ba00aa2e596121fdaf20
ForeverFancy <zhangbowen@mail.ustc.edu.cn>
2018-09-22 22:21:53 +0800

    add time limit and handle the exception

--- count.py
+++ count.py
@@ -4,15 +4,19 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlencode
 import re
 import urllib.request
-
 import datetime
 import threading
 import multiprocessing
 
+
+# count = -1 means TLE
+# count = 0 means there is nothing in the database
 def NCBI_count(term, all_result):  # find all databases which are concerning with the item
     stime = datetime.datetime.now()
-
-    name_dic = {'gquery': 'All Databases', 'assembly': 'Assembly', 'biocollections': 'Biocollections', 'bioproject':
+    timelimit=5
+    result = {}
+    results = []
+    name_dic = {'assembly': 'Assembly', 'biocollections': 'Biocollections', 'bioproject':
         'BioProject', 'biosample': 'BioSample', 'biosystems': 'BioSystems', 'books': 'Books', 'clinvar': 'ClinVar',
                 'clone': 'Clone', 'cdd': 'Conserved Domains', 'gap': 'dbGaP', 'dbvar': 'dbVar', 'nucest': 'EST',
                 'gene': 'Gene', 'genome': 'Genome', 'gds': 'GEO DataSets', 'geoprofiles': 'GEO Profiles', 'nucgss':
@@ -26,33 +30,64 @@ def NCBI_count(term, all_result):  # find all databases which are concerning wit
                     'ToolKitBookgh', 'unigene': 'UniGene'}
     url_query = 'https://eutils.ncbi.nlm.nih.gov/gquery' + '?term=' + term + '&retmode=xml'
     webdata = requests.get(url=url_query).text
+    # if found nothing in NCBI then url is the homepage of NCBI.
+
+    if (datetime.datetime.now() - stime).seconds > timelimit:
+        result = {
+            'title': 'NCBI',
+            'count': -1,
+            'url': 'https://www.ncbi.nlm.nih.gov/',
+        }
+        print("Time Limit Exceed.")
+        return result
+    if webdata is None:
+        result = {
+            'title': 'NCBI',
+            'count': 0,
+            'url': 'https://www.ncbi.nlm.nih.gov/',
+        }
+        return None
     soup = BeautifulSoup(webdata, 'lxml')
     names = soup.select('dbname')
     nums = soup.select('count')
-    result = {}
-    results = []
+
     for name, num in zip(names, nums):
         join_name = name_dic[name.get_text()]
-        result['title'] = join_name
-        result['count'] = int(num.get_text())
-        result['url'] = 'https://www.ncbi.nlm.nih.gov/' + name.get_text() + '/?term=' + term
-        temp_results = json.dumps(result)
-        join_result = json.loads(temp_results)
-        all_result.append(join_result)
-        results.append(join_result)
-        result.clear()
-
-    print('ncbi: %d' % (datetime.datetime.now() - stime).seconds)
+        result = {
+            'title': join_name,
+            'count': int(num.get_text()),
+            'url': 'https://www.ncbi.nlm.nih.gov/' + name.get_text() + '/?term=' + term
+        }
+        results.append(result)
+        all_result.append(result)
+    # print('ncbi: %f' % (datetime.datetime.now() - stime).seconds)
 
     return results
 
 
 def NLM_count(keyword, all_result):
     stime = datetime.datetime.now()
-
+    timelimit = 3
     url = 'https://ghr.nlm.nih.gov/search?query=%s&show=xml&count=1' % keyword
     xml = urllib.request.urlopen(url).read()
+    if (datetime.datetime.now() - stime).seconds > timelimit:
+        result = {
+            'title': 'NLM',
+            'count': -1,
+            'url': 'https://ghr.nlm.nih.gov/search?query=%s' % keyword
+        }
+        print("Time Limit Exceed.")
+        return result
+    if xml is None:
+        result = {
+            'title': 'NLM',
+            'count': 0,
+            'url': 'https://ghr.nlm.nih.gov/search?query=%s' % keyword
+        }
+        return result
     soup = BeautifulSoup(xml, 'lxml')
+    if soup.find('search_results') is None:
+        return None
     count = int(soup.find('search_results').attrs['count'])
     result = {
         'title': 'NLM',
@@ -61,13 +96,13 @@ def NLM_count(keyword, all_result):
     }
     all_result.append(result)
 
-    print('nlm: %d' % (datetime.datetime.now () - stime).seconds)
+    # print('nlm: %f' % (datetime.datetime.now() - stime).seconds)
     return result
 
 
 def RCSB_count(keyword, all_result):
     stime = datetime.datetime.now()
-
+    timelimit = 3
     query_text = """
         <orgPdbQuery>
         <queryType>org.pdb.query.simple.AdvancedKeywordQuery</queryType>
@@ -75,21 +110,39 @@ def RCSB_count(keyword, all_result):
         </orgPdbQuery>""" % keyword
     data = query_text.encode('utf-8')
     headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
     url = 'https://www.rcsb.org/pdb/rest/search'
     req = urllib.request.Request(url, data=data, headers=headers)
+
+    if (datetime.datetime.now() - stime).seconds > timelimit:
+        result = {
+            'title': 'RCSB',
+            'count': -1,
+            'url': 'http://www.rcsb.org/pdb/search/navbarsearch.do?f=&q=%s' % keyword,
+        }
+        print("Time Limit Exceed.")
+        return result
+
     res = urllib.request.urlopen(req).read()
+    if res is None:
+        result = {
+            'title': 'RCSB',
+            'count': -1,
+            'url': 'http://www.rcsb.org/pdb/search/navbarsearch.do?f=&q=%s' % keyword,
+        }
+        return result
+
     count = str(res).count('\\n')
     search_url = 'http://www.rcsb.org/pdb/search/navbarsearch.do?f=&q=%s' % keyword
     result = {
-            'title': 'RCSB',
-            'count': count,
-            'url': search_url,
+        'title': 'RCSB',
+        'count': count,
+        'url': search_url,
     }
     all_result.append(result)
 
-    print('rcsb: %d' % (datetime.datetime.now () - stime).seconds)
+    # print('rcsb: %f' % (datetime.datetime.now() - stime).seconds)
 
     return result
 
@@ -99,41 +152,85 @@ def iGEMParts_count(keyword, all_result):
     stime = datetime.datetime.now()
     url = 'http://parts.igem.org/Special:Search?search=%s' % keyword
     headers = {
-    'accept': 'text / html, application / xhtml + xml, application /    xml; q = 0.9, image / webp, image / apng, * / *;q = 0.8'
+        'accept': 'text / html, application / xhtml + xml, application /    xml; q = 0.9, image / webp, image / apng, * / *;q = 0.8'
     }
     req = urllib.request.Request(url, headers=headers)
+
+    if (datetime.datetime.now() - stime).seconds > 3:
+        result = {
+            'title': 'iGEM Parts',
+            'count': -1,
+            'url': 'http://parts.igem.org/Special:Search?search=%s' % keyword,
+        }
+        print("Time Limit Exceed.")
+        return result
     html = urllib.request.urlopen(req).read()
+
+    if html is None:
+        result = {
+            'title': 'iGEM Parts',
+            'count': 0,
+            'url': 'http://parts.igem.org/Special:Search?search=%s' % keyword,
+        }
+        return result
+
     soup = BeautifulSoup(html, 'lxml')
     div = soup.find(attrs={'class': 'results-info'})
+
+    if div is None:
+        result = {
+            'title': 'iGEM Parts',
+            'count': 0,
+            'url': 'http://parts.igem.org/Special:Search?search=%s' % keyword,
+        }
+        return result
+
     count = int(div.find_all('strong')[-1].text)
     search_url = 'http://parts.igem.org/Special:Search?search=%s' % keyword
     result = {
-            'title': 'iGEM Parts',
-            'count': count,
-            'url': search_url,
+        'title': 'iGEM Parts',
+        'count': count,
+        'url': search_url,
     }
     all_result.append(result)
 
-    print('igem: %d' % (datetime.datetime.now () - stime).seconds)
+    # print('igem: %f' % (datetime.datetime.now() - stime).seconds)
     return result
 
 
 def UniProt_count(keyword, all_result):
     stime = datetime.datetime.now()
-
-    query_string = urlencode({'query':keyword})
+    timelimit=2
+    query_string = urlencode({'query': keyword})
     query_url = 'https://www.uniprot.org/uniprot/?%s&sort=score' % query_string
     response = requests.get(query_url)
-    bs = BeautifulSoup(response.content, features='html.parser')
-    count = int(re.findall("\d+",bs.find('div',class_='main-aside').find('script').text)[0])
-    result = {
+
+    if (datetime.datetime.now() - stime).seconds > timelimit:
+        result = {
             'title': 'UniProt',
-            'count':count,
+            'count': -1,
             'url': query_url,
+        }
+        print("Time Limit Exceed.")
+        return result
+    bs = BeautifulSoup(response.content, features='html.parser')
+    if bs.find('div', class_='main-aside') is None or bs.find('div', class_='main-aside').find('script') is None:
+        result = {
+            'title': 'UniProt',
+            'count': 0,
+            'url': query_url,
+        }
+        return result
+
+    count = int(re.findall("\d+", bs.find('div', class_='main-aside').find('script').text)[0])
+    result = {
+        'title': 'UniProt',
+        'count': count,
+        'url': query_url,
     }
     all_result.append(result)
 
-    print('uniprot: %d' % (datetime.datetime.now () - stime).seconds)
+    # print('uniprot: %f' % (datetime.datetime.now() - stime).seconds)
     return result
 
 
@@ -143,16 +240,41 @@ def Taxonomy_count(keyword, all_result):
     query_string = urlencode({'query': keyword})
     query_url = 'https://www.uniprot.org/taxonomy/?%s&sort=score' % query_string
     response = requests.get(query_url)
-    bs = BeautifulSoup(response.content, features='html.parser')
-    count = int(re.findall("\d+",bs.find('div',class_='main-aside').find('script').text)[0])
-    result = {
+    if (datetime.datetime.now() - stime).seconds > 2:
+        result = {
             'title': 'Taxonomy',
-            'count':count,
+            'count': -1,
             'url': query_url,
+        }
+        print("Time Limit Exceed.")
+        return result
+
+    if response is None:
+        result = {
+            'title': 'Taxonomy',
+            'count': 0,
+            'url': query_url,
+        }
+        return result
+
+    bs = BeautifulSoup(response.content, features='html.parser')
+    if bs.find('div', class_='main-aside').find('script') is None:
+        result = {
+            'title': 'Taxonomy',
+            'count': 0,
+            'url': query_url,
+        }
+        return result
+
+    count = int(re.findall("\d+", bs.find('div', class_='main-aside').find('script').text)[0])
+    result = {
+        'title': 'Taxonomy',
+        'count': count,
+        'url': query_url,
     }
     all_result.append(result)
 
-    print('tax: %d' % (datetime.datetime.now () - stime).seconds)
+    # print('tax: %f' % (datetime.datetime.now() - stime).seconds)
     return result
 
 
@@ -170,34 +292,41 @@ class Spiders(threading.Thread):
             self.process_pool.append(multiprocessing.Process(target=spider, args=(self.keyword, [])))
 
     def count_compare(self):
-        stime = datetime.datetime.now ()
+        stime = datetime.datetime.now()
+        print(stime)
         for spider in self.all_spiders:
             spider(self.keyword, [])
-        print('All time cost: %d' % (datetime.datetime.now () - stime).seconds)
+        print('All time cost: %f' % (datetime.datetime.now() - stime).seconds)
 
     def count_multithread(self):
         stime = datetime.datetime.now()
+        total_timeout = 5  # total time limit
         for th in self.thread_pool:
+            th.setDaemon(True)
             th.start()
+
         for th in self.thread_pool:
-            threading.Thread.join(th)
-        print('All time cost: %d' % (datetime.datetime.now() - stime).seconds)
+
+            if (datetime.datetime.now() - stime).seconds > total_timeout:
+                raise TimeoutError("Timeout")
+            threading.Thread.join(th, timeout=1)  # single thread time limit
+
+        print('All time cost: %f' % (datetime.datetime.now() - stime).seconds)
         return self.results
 
     def count_multiproccess(self):
-        stime = datetime.datetime.now ()
+        stime = datetime.datetime.now()
+        total_timeout = 5
         for th in self.process_pool:
             th.start()
+
         for th in self.process_pool:
-            multiprocessing.Process.join(th)
-        print('All time cost: %d' % (datetime.datetime.now () - stime).seconds)
+            if (datetime.datetime.now() - stime).seconds > total_timeout:
+                print("Time Limit Exceed.")
+            multiprocessing.Process.join(th, timeout=1)
+        print('All time cost: %d' % (datetime.datetime.now() - stime).seconds)
 
 
 if __name__ == "__main__":
-    s = Spiders('p53')
-    print('Time cost for compare:')
-    s.count_compare()
-    print('Time cost using multiprocessing:')
-    s.count_multiproccess()
-    print('Time cost using multithreading:')
+    s = Spiders(input("Input the term: "))
     print(s.count_multithread())
